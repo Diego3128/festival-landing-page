d068dc0c7dcbe686f218145c9b48ce63641947cc
DiegoFabian <145170018+Diego3128@users.noreply.github.com>
2024-08-27 16:26:27 -0500

    img thumbnails

--- gulpFile.js
+++ gulpFile.js
@@ -1,7 +1,11 @@
+import fs from "fs";
+import path from "path";
 import { src, dest, watch, series } from "gulp";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
+
 import terser from "gulp-terser";
+import sharp from "sharp";
 
 const sass = gulpSass(dartSass);
 
@@ -17,9 +21,40 @@ export function compileCss(done) {
     .pipe(dest("./build/css", { sourcemaps: "." }));
   done();
 }
+
+
+
+export async function crop(done) {
+  const inputFolder = 'src/img/gallery/full'
+  const outputFolder = 'src/img/gallery/thumb';
+  const width = 250;
+  const height = 180;
+  if (!fs.existsSync(outputFolder)) {
+      fs.mkdirSync(outputFolder, { recursive: true })
+  }
+  const images = fs.readdirSync(inputFolder).filter(file => {
+      return /\.(jpg)$/i.test(path.extname(file));
+  });
+  try {
+      images.forEach(file => {
+          const inputFile = path.join(inputFolder, file)
+          const outputFile = path.join(outputFolder, file)
+          sharp(inputFile) 
+              .resize(width, height, {
+                  position: 'centre'
+              })
+              .toFile(outputFile)
+      });
+
+      done()
+  } catch (error) {
+      console.log(error)
+  }
+}
+
 export function dev() {
   watch("./src/scss/**/*.scss", compileCss);
   watch("./src/js/**/*.js", js);
 }
 
-export default series(js, compileCss, dev);
+export default series(crop, js, compileCss, dev);
